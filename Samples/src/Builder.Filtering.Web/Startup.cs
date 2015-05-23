8dc9c5f7d2a20f3bcd361bbee290f9e15c3d0987
BoyceLyu <lvzhendong.bl@gmail.com>
2015-05-24 07:52:08 +0800

    Builder Samples

---
+++ Samples/src/Builder.Filtering.Web/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace Builder.Filtering.Web
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Use(next => context => Filter(context, next));
+
+            app.Run(HelloBoyceAsync);
+        }
+
+        public async Task Filter(HttpContext context, RequestDelegate next)
+        {
+            var body = context.Response.Body;
+            var buffer = new MemoryStream();
+            context.Response.Body = buffer;
+
+            try
+            {
+                context.Response.Headers["CustomHeader"] = "My Header";
+
+                await context.Response.WriteAsync("Before \r\n");
+                await next(context);
+                await context.Response.WriteAsync("After \r\n");
+
+                buffer.Position = 0;
+                await buffer.CopyToAsync(body);
+            }
+            finally
+            {
+                context.Response.Body = body;
+            }
+        }
+
+        public async Task HelloBoyceAsync(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            await context.Response.WriteAsync("Hello BoyceLyu!");
+        }
+    }
+}
---
+++ Samples/src/Builder.Middleware.Web/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace Builder.Middleware.Web
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseXHttpHeaderOverride();
+
+            app.UseMiddleware(typeof (MyMiddleware), "No");
+        }
+    }
+
+    public static class BuilderExtensions
+    {
+        public static IApplicationBuilder UseXHttpHeaderOverride(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<XhttpHeaderOverrideMiddleware>();
+        }
+    }
+
+    public class XhttpHeaderOverrideMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public XhttpHeaderOverrideMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            var headerValue = httpContext.Request.Headers["X-HTTP-Method-Override"];
+            var queryValue = httpContext.Request.Query["X-HTTP-Method-Override"];
+
+            if (!string.IsNullOrEmpty(headerValue))
+            {
+                httpContext.Request.Method = headerValue;
+            }
+            else if(!string.IsNullOrEmpty(queryValue))
+            {
+                httpContext.Request.Method = queryValue;
+            }
+
+            return _next.Invoke(httpContext);
+        }
+    }
+
+    public class MyMiddleware
+    {
+        private RequestDelegate _next;
+        private string _greeting;
+        private IServiceProvider _service;
+
+        public MyMiddleware(RequestDelegate next, string greeting, IServiceProvider service)
+        {
+            _next = next;
+            _greeting = greeting;
+            _service = service;
+        }
+
+        public async Task Invoke(HttpContext httpcontext)
+        {
+            await httpcontext.Response.WriteAsync(_greeting + ", middleware!\r\n");
+            await httpcontext.Response.WriteAsync("This request is a " + httpcontext.Request.Method + "\r\n");
+        }
+    }
+}
