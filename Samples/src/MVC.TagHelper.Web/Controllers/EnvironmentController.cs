6120973c34fe6b103f4943bddefd01b6024cd2a2
BoyceLyu <lvzhendong.bl@gmail.com>
2015-05-29 23:58:18 +0800

    Add Environment Tag Helper demo

---
+++ Samples/src/MVC.TagHelper.Web/Controllers/EnvironmentController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace MVC.TagHelper.Web.Controllers
+{
+    public class EnvironmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
