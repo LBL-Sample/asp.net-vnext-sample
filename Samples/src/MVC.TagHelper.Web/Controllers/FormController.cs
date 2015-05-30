968eac227087c027af99b6b35e7c104e195d946c
BoyceLyu <lvzhendong.bl@gmail.com>
2015-05-30 23:42:34 +0800

    update

--- Samples/src/MVC.TagHelper.Web/Controllers/FormController.cs
+++ Samples/src/MVC.TagHelper.Web/Controllers/FormController.cs
@@ -4,20 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 
-
 namespace MVC.TagHelper.Web.Controllers
 {
     public class FormController : Controller
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Post()
-        {
-            
             return View();
         }
     }
